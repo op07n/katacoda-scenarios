6bff1065e92e8ba540906fb4dd9d322787a9f3a1
op07n <39182105+op07n@users.noreply.github.com>
2019-10-08 16:12:01 +0200

    Update foreground.sh

--- node-red-playground-next/foreground.sh
+++ node-red-playground-next/foreground.sh
@@ -1,4 +1,6 @@
 sudo npm install -g --unsafe-perm node-red@next
+sudo npm install -g --unsafe-perm node-red-contrib-fs
+sudo npm install -g --unsafe-perm node-red-contrib-http-proxy
 sudo npm install -g --unsafe-perm node-red-dashboard
 sudo npm install -g --unsafe-perm node-red-contrib-web-worldmap
 sudo npm install -g --unsafe-perm node-red-contrib-flightradar24
