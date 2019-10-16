bb6ff381267e20cf022e90adf62fe85755aaa61d
op07n <39182105+op07n@users.noreply.github.com>
2019-10-16 08:47:38 +0200

    Create foreground.sh

---
+++ streamlitplayground/foreground.sh
@@ -0,0 +1,3 @@
+sudo pip3 install jsonschema
+sudo pip3 install --upgrade streamlit
+streamlit run https://gist.githubusercontent.com/treuille/c633dc8bc86efaa98eb8abe76478aa81/raw/2019640b6a9ff5da5ab6d5b11b3345ddc764b285/cache_example.py
