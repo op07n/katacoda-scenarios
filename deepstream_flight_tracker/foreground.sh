290aacfa8056dbe1bd60c2cfb8bd255a727f5184
op07n <39182105+op07n@users.noreply.github.com>
2019-10-30 09:14:35 +0100

    Update foreground.sh

--- deepstream_flight_tracker/foreground.sh
+++ deepstream_flight_tracker/foreground.sh
@@ -1,4 +1,4 @@
 sudo git clone https://github.com/deepstreamIO/dsh-demo-realtime-flight-tracker.git
-cd dsh-demo-realtime-flight-tracker
+cd dsh-demo-realtime-flight-tracker/
 sudo npm install
 sudo npm run dev
