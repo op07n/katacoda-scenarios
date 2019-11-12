2a50779f66dcf48ffb2265a35dec72210bb215d7
op07n <39182105+op07n@users.noreply.github.com>
2019-11-12 16:45:10 +0100

    Update foreground.sh

--- wine-x11-novnc-docker/foreground.sh
+++ wine-x11-novnc-docker/foreground.sh
@@ -1,6 +1,6 @@
 # dpkg --add-architecture i386
 # apt-get update && apt-get -y install xvfb x11vnc xdotool wget tar supervisor wine32-development net-tools fluxbox
-apt-get update && apt-get -y install xvfb x11vnc xdotool firefox; \
+apt-get update && apt-get -y install xvfb x11vnc; \
 wget -O - https://github.com/novnc/noVNC/archive/v1.1.0.tar.gz | tar -xzv -C /root/ && mv /root/noVNC-1.1.0 /root/novnc && ln -s /root/novnc/vnc_lite.html /root/novnc/index.html; \
 wget -O - https://github.com/novnc/websockify/archive/v0.8.0.tar.gz | tar -xzv -C /root/ && mv /root/websockify-0.8.0 /root/novnc/utils/websockify; \
 export DISPLAY=:1 ; (Xvfb :1 -screen 0 1024x768x24 &) ;\ 
