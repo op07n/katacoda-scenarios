e39d5550fb790f0f2c8ada71a5225c3ed847ee48
op07n <39182105+op07n@users.noreply.github.com>
2019-11-12 10:56:58 +0100

    Update foreground.sh

--- wine-x11-novnc-docker/foreground.sh
+++ wine-x11-novnc-docker/foreground.sh
@@ -9,4 +9,4 @@ export DISPLAY=:1 ; \
 (sleep "5");\
 # (openbox-session& );\
 (x11vnc -display :1 -nopw -listen localhost -xkb -ncache 10 -ncache_cr -forever & );\
-cd /root/noVNC && ln -s vnc_auto.html index.html && ./utils/launch.sh --vnc localhost:5900 ; 
+cd /root/noVNC && ln -s vnc_auto.html index.html && ./utils/launch.sh --vnc localhost:5900 --listen 8080;
