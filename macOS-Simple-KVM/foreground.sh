58e303c311297139fbd8f36443edd5578ff27460
op07n <39182105+op07n@users.noreply.github.com>
2019-11-12 13:36:22 +0100

    Update foreground.sh

--- macOS-Simple-KVM/foreground.sh
+++ macOS-Simple-KVM/foreground.sh
@@ -1,7 +1,8 @@
 git clone https://github.com/foxlet/macOS-Simple-KVM.git; \
 cd macOS-Simple-KVM; \
-apt-get install qemu-system qemu-utils; \
+apt-get -y install qemu-system qemu-utils; \
 ./jumpstart.sh --mojave; \
-echo '-drive id=SystemDisk,if=none,file=MyDisk.qcow2' >> basic.sh; \
-echo '-device ide-hd,bus=sata.4,drive=SystemDisk' >> basic.sh; \
-basic.sh; 
+qemu-img create -f qcow2 MyDisk.qcow2 64G; \
+echo '    -drive id=SystemDisk,if=none,file=MyDisk.qcow2 \' >> basic.sh; \
+echo '    -device ide-hd,bus=sata.4,drive=SystemDisk \' >> basic.sh; \
+./basic.sh; 
