f4b9ef92c458b06112f97ff5145fb4d68e2bc702
op07n <39182105+op07n@users.noreply.github.com>
2019-11-24 10:29:29 +0100

    Update foreground.sh

--- docker-arm/foreground.sh
+++ docker-arm/foreground.sh
@@ -1,1 +1,1 @@
-docker run --rm --privileged hypriot/qemu-register; docker run -it dieterreuter/alpine-arm64:3.9;
+docker run --rm --privileged hypriot/qemu-register; docker run -it dieterreuter/alpine-arm64:3.9;apk update;apk add gcc-gnat;
