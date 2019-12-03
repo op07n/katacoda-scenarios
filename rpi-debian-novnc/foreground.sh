2eba361d88cdc097d5b8f82805b0014d097c3c11
op07n <39182105+op07n@users.noreply.github.com>
2019-12-03 07:53:22 +0100

    Update foreground.sh

--- rpi-debian-novnc/foreground.sh
+++ rpi-debian-novnc/foreground.sh
@@ -1,1 +1,1 @@
-sudo docker run --rm --privileged hypriot/qemu-register; sudo docker run --rm -it -p 8080:8080 op07n/rpidebiannovnc:part1;
+# sudo docker run --rm --privileged hypriot/qemu-register; sudo docker run --rm -it -p 8080:8080 op07n/rpidebiannovnc:part1;
