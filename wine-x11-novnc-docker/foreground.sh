2c0f2019903c83beaea13404952b09b4c458736a
op07n <39182105+op07n@users.noreply.github.com>
2019-12-19 14:07:48 +0100

    Update foreground.sh

--- wine-x11-novnc-docker/foreground.sh
+++ wine-x11-novnc-docker/foreground.sh
@@ -3,8 +3,10 @@
 apt-get update && apt-get -y install xvfb x11vnc; \
 wget -O - https://github.com/novnc/noVNC/archive/v1.1.0.tar.gz | tar -xzv -C /root/ && mv /root/noVNC-1.1.0 /root/novnc && ln -s /root/novnc/vnc_lite.html /root/novnc/index.html; \
 wget -O - https://github.com/novnc/websockify/archive/v0.8.0.tar.gz | tar -xzv -C /root/ && mv /root/websockify-0.8.0 /root/novnc/utils/websockify; \
-(export DISPLAY=:1) ; \
-(Xvfb :1 -screen 0 1024x768x24 &) ; \ 
+sudo export DISPLAY=:1 ; \
+echo $DISPLAY
+sudo Xvfb :1 -screen 0 1024x768x24 & ; \ 
+echo "sleep"
 (sleep 5) ; \
 (x11vnc -display :1 -nopw -listen localhost -xkb -ncache 10 -ncache_cr -forever &) ; \
 (cd novnc && ./utils/launch.sh --vnc localhost:5900 --listen 8080 );
