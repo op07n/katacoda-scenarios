2e85016971631b0a061fd9fc18af999c6d6a41b5
op07n <39182105+op07n@users.noreply.github.com>
2020-02-14 20:21:59 +0100

    Update foreground.sh

--- dotnet30/foreground.sh
+++ dotnet30/foreground.sh
@@ -1,5 +1,5 @@
-wget -q https://packages.microsoft.com/config/ubuntu/18.04/packages-microsoft-prod.deb && \
+wget -q https://packages.microsoft.com/config/ubuntu/19.04/packages-microsoft-prod.deb && \
     dpkg -i packages-microsoft-prod.deb && rm -rf packages-microsoft-prod.deb && \
     add-apt-repository universe && \
-    apt-get update && apt-get -y -o APT::Install-Suggests="true" install dotnet-sdk-3.0 && \
-    apt -y clean;
+    apt-get update && apt-get -y -o APT::Install-Suggests="true" install dotnet-sdk-3.1 && \
+    rm -rf /var/lib/apt/lists/*;
